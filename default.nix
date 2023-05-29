1adc8c410e9269eecfd3855c8643669623e64ac0
FlafyDev <flafyarazi@gmail.com>
2023-05-30 00:31:23 +0300

    fixed submodules

--- default.nix
+++ default.nix
@@ -15,10 +15,9 @@
       if builtins.pathExists flakeFile
       then
         (import ./entry.nix {
-          pkgs = {};
-          config = {};
-          inherit lib modules;
-          inputs = {};
+          inherit modules;
+          inputs = {nixpkgs.lib = lib;};
+          system = builtins.currentSystem;
         })
         .config
         .inputs
@@ -33,118 +32,110 @@
     system,
     modules,
   }: let
-    inherit (inputs.nixpkgs) lib;
-    inherit
-      ((inputs.nixpkgs.lib.evalModules {
-        specialArgs = {
-          pkgs =
-            inputs.nixpkgs.legacyPackages.${system};
-        };
-        modules = import "${inputs.nixpkgs}/nixos/modules/module-list.nix";
-      }))
-      options
-      ;
-    inherit
-      ((import ./entry.nix {
-          pkgs = {};
-          config = {};
-          inherit modules inputs lib;
-        })
-        .config)
-      sysModules
-      homeModules
-      ;
-    recursiveSetIfDefined = attrs: prevName:
-      lib.mapAttrs (
-        name: value: let
-          newName = "${prevName}.${name}";
-        in
-          if (value ? "isDefined")
-          then (lib.mkIf value.isDefined value.value)
-          else recursiveSetIfDefined value newName
-      )
-      (lib.filterAttrs (name: value: (!(value ? "readOnly" && value.readOnly))) attrs);
-    recursivePrintIfDefined = attrs: prevName:
-      lib.mapAttrs (
-        name: value: let
-          newName = "${prevName}.${name}";
-        in
-          if (value ? "isDefined")
-          then
-            (
-              if value.isDefined
-              then builtins.trace "setting ${newName} to '${toString value.value}'" value.value
-              else null
-            )
-          else recursiveSetIfDefined value newName
-      )
-      (lib.filterAttrs (name: value: (!(value ? "readOnly" && value.readOnly))) attrs);
-  in
-    inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules =
-        sysModules
-        ++ [
-          inputs.home-manager.nixosModules.home-manager
-          ({
-            pkgs,
-            lib,
-            config,
-            ...
-          }: let
-            res = import ./entrytest.nix {
-              inherit pkgs lib config inputs modules options;
-            };
-          in
-            builtins.trace
-            # ((recursiveSetIfDefined res.options.sys "root")
-            #   // {
-            #     _module.args = {
-            #       pkgs = lib.mkForce (import inputs.nixpkgs (
-            #         (builtins.removeAttrs config.nixpkgs ["localSystem"])
-            #         // {
-            #           overlays = config.nixpkgs.overlays ++ res.config.nixpkgs.overlays;
-            #           config = config.nixpkgs.config // res.config.nixpkgs.config;
-            #         }
-            #       ));
-            #     };
-            #     # home-manager.useGlobalPkgs =  recursivePrintIfDefined res.options.sys "root" == {};
-            #     home-manager.useGlobalPkgs = true;
-            #     home-manager.useUserPackages = true;
-            #     home-manager.sharedModules = homeModules;
-            #     home-manager.users.${res.config.home.home.username} = _: {config = res.config.home;};
-            #     # users.users.root.uid = null;
-            #     # system.build = lib.mkIf false null;
-            #     # users.users.root.group = lib.mkIf false null;
-            #     # users.users.root.password = lib.mkIf false null;
-            #     # users.users.root.isSystemUser = lib.mkIf false null;
-            #   }).users.users.content.root.password
-              ( options.users.users.type.getSubOptions []).root
-              {
-              config =
-                (recursiveSetIfDefined res.options.sys "root")
-                // {
-                  _module.args = {
-                    pkgs = lib.mkForce (import inputs.nixpkgs (
-                      (builtins.removeAttrs config.nixpkgs ["localSystem"])
-                      // {
-                        overlays = config.nixpkgs.overlays ++ res.config.nixpkgs.overlays;
-                        config = config.nixpkgs.config // res.config.nixpkgs.config;
-                      }
-                    ));
-                  };
-                  # home-manager.useGlobalPkgs =  recursivePrintIfDefined res.options.sys "root" == {};
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.sharedModules = homeModules;
-                  home-manager.users.${res.config.home.home.username} = _: {config = res.config.home;};
-                  # users.users.root.uid = null;
-                  # system.build = lib.mkIf false null;
-                  # users.users.root.group = lib.mkIf false null;
-                  # users.users.root.password = lib.mkIf false null;
-                  # users.users.root.isSystemUser = lib.mkIf false null;
-                };
-            })
-        ];
-    };
+    inherit ((import ./entry.nix {inherit modules inputs system;}).config) osModules;
+    # inherit
+    #   ((inputs.nixpkgs.lib.evalModules {
+    #     specialArgs = {
+    #       pkgs =
+    #         inputs.nixpkgs.legacyPackages.${system};
+    #     };
+    #     modules = import "${inputs.nixpkgs}/nixos/modules/module-list.nix";
+    #   }))
+    #   options
+    #   ;
+    # recursiveSetIfDefined = attrs: prevName:
+    #   lib.mapAttrs (
+    #     name: value: let
+    #       newName = "${prevName}.${name}";
+    #     in
+    #       if (value ? "isDefined")
+    #       then (lib.mkIf value.isDefined value.value)
+    #       else recursiveSetIfDefined value newName
+    #   )
+    #   (lib.filterAttrs (name: value: (!(value ? "readOnly" && value.readOnly))) attrs);
+    # recursivePrintIfDefined = attrs: prevName:
+    #   lib.mapAttrs (
+    #     name: value: let
+    #       newName = "${prevName}.${name}";
+    #     in
+    #       if (value ? "isDefined")
+    #       then
+    #         (
+    #           if value.isDefined
+    #           then builtins.trace "setting ${newName} to '${toString value.value}'" value.value
+    #           else null
+    #         )
+    #       else recursiveSetIfDefined value newName
+    #   )
+    #   (lib.filterAttrs (name: value: (!(value ? "readOnly" && value.readOnly))) attrs);
+
+    evaluated = import ./entry.nix {inherit modules inputs osModules system;};
+  in {config = evaluated.config.os;};
+  # inputs.nixpkgs.lib.nixosSystem {
+  #   inherit system;
+  #   modules =
+  #     sysModules
+  #     ++ [
+  #       inputs.home-manager.nixosModules.home-manager
+  #       ({
+  #         pkgs,
+  #         lib,
+  #         config,
+  #         ...
+  #       }: let
+  #         res = import ./entrytest.nix {
+  #           inherit pkgs lib config inputs modules options;
+  #         };
+  #       in
+  #         # builtins.trace
+  #         # # ((recursiveSetIfDefined res.options.sys "root")
+  #         # #   // {
+  #         # #     _module.args = {
+  #         # #       pkgs = lib.mkForce (import inputs.nixpkgs (
+  #         # #         (builtins.removeAttrs config.nixpkgs ["localSystem"])
+  #         # #         // {
+  #         # #           overlays = config.nixpkgs.overlays ++ res.config.nixpkgs.overlays;
+  #         # #           config = config.nixpkgs.config // res.config.nixpkgs.config;
+  #         # #         }
+  #         # #       ));
+  #         # #     };
+  #         # #     # home-manager.useGlobalPkgs =  recursivePrintIfDefined res.options.sys "root" == {};
+  #         # #     home-manager.useGlobalPkgs = true;
+  #         # #     home-manager.useUserPackages = true;
+  #         # #     home-manager.sharedModules = homeModules;
+  #         # #     home-manager.users.${res.config.home.home.username} = _: {config = res.config.home;};
+  #         # #     # users.users.root.uid = null;
+  #         # #     # system.build = lib.mkIf false null;
+  #         # #     # users.users.root.group = lib.mkIf false null;
+  #         # #     # users.users.root.password = lib.mkIf false null;
+  #         # #     # users.users.root.isSystemUser = lib.mkIf false null;
+  #         # #   }).users.users.content.root.password
+  #         # (options.users.users.type.getSubOptions []).root
+  #         # {
+  #         #   config =
+  #         #     (recursiveSetIfDefined res.options.sys "root")
+  #         #     // {
+  #         #       _module.args = {
+  #         #         pkgs = lib.mkForce (import inputs.nixpkgs (
+  #         #           (builtins.removeAttrs config.nixpkgs ["localSystem"])
+  #         #           // {
+  #         #             overlays = config.nixpkgs.overlays ++ res.config.nixpkgs.overlays;
+  #         #             config = config.nixpkgs.config // res.config.nixpkgs.config;
+  #         #           }
+  #         #         ));
+  #         #       };
+  #         #       # home-manager.useGlobalPkgs =  recursivePrintIfDefined res.options.sys "root" == {};
+  #         #       home-manager.useGlobalPkgs = true;
+  #         #       home-manager.useUserPackages = true;
+  #         #       home-manager.sharedModules = homeModules;
+  #         #       home-manager.users.${res.config.home.home.username} = _: {config = res.config.home;};
+  #         #       # users.users.root.uid = null;
+  #         #       # system.build = lib.mkIf false null;
+  #         #       # users.users.root.group = lib.mkIf false null;
+  #         #       # users.users.root.password = lib.mkIf false null;
+  #         #       # users.users.root.isSystemUser = lib.mkIf false null;
+  #         #     };
+  #         # })
+  #     ];
+  # };
 }
--- entry.nix
+++ entry.nix
@@ -1,79 +1,228 @@
 {
-  lib,
-  pkgs,
-  config,
-  options ? {},
-  modules,
+  modules ? [],
   inputs,
-  osConfig ? {},
-}:
-lib.evalModules {
-  specialArgs = {
-    inherit pkgs lib inputs;
-    sysConfig = config;
-    modulesPath = builtins.toString ./modules;
-  };
-  modules =
-    [
-      ({lib, ...}: let
-        inherit (lib) mkOption types;
-        moreTypes = import ./types.nix {inherit lib;};
-      in {
-        options = {
-          sys = mkOption {
-            type = moreTypes.anything;
+  system,
+  osModules ? [],
+}: let
+  inherit (inputs.nixpkgs) lib;
+  inherit (lib) mkOption types evalModules;
+  osModule = let
+    baseModules = import "${inputs.nixpkgs}/nixos/modules/module-list.nix";
+  in
+    types.submoduleWith {
+      description = "Home Manager module";
+      specialArgs = {
+        inherit baseModules;
+        modulesPath = "${inputs.nixpkgs}/nixos/modules";
+      };
+      modules =
+        baseModules
+        ++ osModules
+        ++ [
+          {nixpkgs.system = system;}
+        ];
+    };
+in
+  evalModules {
+    specialArgs = {
+      inherit inputs;
+    };
+    modules =
+      [
+        # TODO
+        {
+          os.system.nixos.versionSuffix = ".dirty";
+          os.system.nixos.revision = "dirty";
+        }
+
+        {
+          options = {
+            inputs = mkOption {
+              type = with types; attrs;
+              default = {};
+              description = "Inputs";
+            };
+          };
+        }
+
+        # Home Manager
+        ({
+          options,
+          config,
+          osConfig,
+          lib,
+          ...
+        }: {
+          options.hm = lib.mkOption {
+            type = lib.types.deferredModule;
             default = {};
+            description = ''
+              Home Manager configuration.
+            '';
           };
 
-          sysModules = mkOption {
-            type = with types; listOf deferredModule;
-            default = [];
-            # example = literalExpression "[ pkgs.vim ]";
-            # description = "";
+          options.hmUsername = lib.mkOption {
+            type = lib.types.str;
+            default = "user";
+            description = ''
+              Username used for hm.
+            '';
           };
 
-          home = mkOption {
-            type = moreTypes.anything;
-            default = {};
-            # example = literalExpression "[ pkgs.vim ]";
-            # description = "";
-          };
-
-          homeModules = mkOption {
+          options.hmModules = lib.mkOption {
             type = with types; listOf raw;
             default = [];
-            # example = literalExpression "[ pkgs.vim ]";
-            # description = "";
+            description = "Home Manager modules.";
           };
 
-          inputs = mkOption {
-            type = with types; attrs;
-            default = {};
-            # example = literalExpression "[ pkgs.vim ]";
-            # description = "";
+          config = {
+            _module.args.hmConfig = osConfig.home-manager.users.${config.hmUsername};
+            osModules = [inputs.home-manager.nixosModules.home-manager];
+            os = {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${config.hmUsername} = config.hm;
+              home-manager.sharedModules = config.hmModules;
+              home-manager.extraSpecialArgs = {inherit inputs;};
+            };
+          };
+        })
+
+        # Nixpkgs modules
+        ({
+          config,
+          osConfig,
+          ...
+        }: {
+          options = {
+            os = lib.mkOption {
+              type = osModule;
+              default = {};
+              visible = "shallow";
+              description = ''
+                Nixpkgs configuration.
+              '';
+            };
+
+            osModules = mkOption {
+              type = with types; listOf raw;
+              default = [];
+              description = "Top level system modules.";
+            };
           };
 
-          nixpkgs.config = mkOption {
-            type = with types;
-              if options ? nixpkgs
-              then options.nixpkgs.config.type
-              else attrs;
-            default = {};
-            # example = literalExpression "[ pkgs.vim ]";
-            # description = "";
+          config = {
+            _module.args =
+              config.os._combined-manager.args
+              // {
+                osConfig = config.os;
+              };
+            os = {pkgs, ...}: {
+              options = {
+                _combined-manager.args = lib.mkOption {
+                  type = lib.types.attrs;
+                  default = {};
+                  visible = "hidden";
+                };
+              };
+              config._combined-manager.args = {inherit pkgs;};
+            };
           };
+        })
 
-          nixpkgs.overlays = mkOption {
-            type = with types;
-              if options ? nixpkgs
-              then options.nixpkgs.overlays.type
-              else listOf anything;
-            default = [];
-            # example = literalExpression "[ pkgs.vim ]";
-            # description = "";
-          };
-        };
-      })
-    ]
-    ++ modules;
-}
+        # (_: {
+        #   imports = [
+        #     ({pkgs, ...}: {
+        #       os.nixpkgs.overlays = [
+        #         (final: prev: {
+        #           customVim = prev.vim;
+        #         })
+        #       ];
+        #     })
+        #   ];
+        #
+        #   config = {
+        #     os = builtins.trace utils {
+        #       programs.hyprland.enable = true;
+        #       users.users.root = {
+        #         group = "root";
+        #         password = "root";
+        #         isSystemUser = true;
+        #       };
+        #       users.users.a = {
+        #         group = "a";
+        #         password = "a";
+        #         isNormalUser = true;
+        #       };
+        #       users.mutableUsers = false;
+        #
+        #       # environment.systemPackages = [
+        #       #   pkgs.customVim
+        #       # ];
+        #     };
+        #     hm.home.stateVersion = "22.05";
+        #   };
+        # })
+
+        #   ({lib, ...}: let
+        #     inherit (lib) mkOption types;
+        #     moreTypes = import ./types.nix {inherit lib;};
+        #   in {
+        #     options = {
+        #       sys = mkOption {
+        #         type = moreTypes.anything;
+        #         default = {};
+        #       };
+        #
+        #       sysModules = mkOption {
+        #         type = with types; listOf deferredModule;
+        #         default = [];
+        #         # example = literalExpression "[ pkgs.vim ]";
+        #         # description = "";
+        #       };
+        #
+        #       home = mkOption {
+        #         type = moreTypes.anything;
+        #         default = {};
+        #         # example = literalExpression "[ pkgs.vim ]";
+        #         # description = "";
+        #       };
+        #
+        #       homeModules = mkOption {
+        #         type = with types; listOf raw;
+        #         default = [];
+        #         # example = literalExpression "[ pkgs.vim ]";
+        #         # description = "";
+        #       };
+        #
+        #       inputs = mkOption {
+        #         type = with types; attrs;
+        #         default = {};
+        #         # example = literalExpression "[ pkgs.vim ]";
+        #         # description = "";
+        #       };
+        #
+        #       nixpkgs.config = mkOption {
+        #         type = with types;
+        #           if options ? nixpkgs
+        #           then options.nixpkgs.config.type
+        #           else attrs;
+        #         default = {};
+        #         # example = literalExpression "[ pkgs.vim ]";
+        #         # description = "";
+        #       };
+        #
+        #       nixpkgs.overlays = mkOption {
+        #         type = with types;
+        #           if options ? nixpkgs
+        #           then options.nixpkgs.overlays.type
+        #           else listOf anything;
+        #         default = [];
+        #         # example = literalExpression "[ pkgs.vim ]";
+        #         # description = "";
+        #       };
+        #     };
+        #   })
+      ]
+      ++ modules;
+  }
