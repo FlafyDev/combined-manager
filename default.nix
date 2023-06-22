32de9c058f13f8d78f2ff5c9a06691801915466d
FlafyDev <flafyarazi@gmail.com>
2023-06-22 12:57:31 +0300

    replace parameter root with lockFile in mkInputs
    remove parameter initialInputs from mkInputs

--- default.nix
+++ default.nix
@@ -2,39 +2,30 @@ let
   evalModules = import ./eval-modules.nix;
 in {
   mkInputs = {
-    root,
-    initialInputs,
+    lockFile,
     modules,
   }: let
-    flakeFile = root + "/flake.lock";
-    nixpkgsLock = (builtins.fromJSON (builtins.readFile flakeFile)).nodes.nixpkgs.locked;
+    nixpkgsLock = (builtins.fromJSON (builtins.readFile lockFile)).nodes.nixpkgs.locked;
     lib = import ((builtins.fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsLock.rev}.tar.gz";
         sha256 = nixpkgsLock.narHash;
       })
       + "/lib");
-    additionalInputs =
-      if builtins.pathExists flakeFile
-      then
-        (evalModules {
-          inherit modules;
-          inputs = {
-            nixpkgs = {
-              inherit lib;
-              inherit (nixpkgsLock) rev;
-              lastModified = toString nixpkgsLock.lastModified;
-              shortRev = builtins.substring 0 7 nixpkgsLock.rev;
-            };
-          };
-          system = builtins.currentSystem;
-        })
-        .config
-        .inputs
-      else {};
   in
-    assert builtins.elem "nixpkgs" (builtins.attrNames initialInputs) || throw "nixpkgs input not found in initialInputs" {};
-    assert builtins.elem "home-manager" (builtins.attrNames initialInputs) || throw "home-manager input not found in initialInputs" {};
-      additionalInputs // initialInputs;
+    (evalModules {
+      inherit modules;
+      inputs = {
+        nixpkgs = {
+          inherit lib;
+          inherit (nixpkgsLock) rev;
+          lastModified = toString nixpkgsLock.lastModified;
+          shortRev = builtins.substring 0 7 nixpkgsLock.rev;
+        };
+      };
+      system = builtins.currentSystem;
+    })
+    .config
+    .inputs;
 
   mkNixosSystem = {
     inputs,
