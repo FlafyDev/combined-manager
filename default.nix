20626d1609853b62382034b99ce9d17b780a854e
Noah765 <noland62007@gmail.com>
2024-07-05 14:47:57 +0200

    Restructuring

--- default.nix
+++ default.nix
@@ -1,121 +1,122 @@
 let
-# TODO A proper input type
-inputType = types: with types; attrsOf (uniq anything);
-  
+  # TODO A proper input type
+  getInputType = types: with types; attrsOf (uniq anything);
+
   evalModules =
     {
-      system ? builtins.currentSystem,
       prefix ? [ ],
       specialArgs ? { },
       useHomeManager ? true,
       modules,
       osModules ? [ ],
       hmModules ? [ ],
-    }@args:
+    }:
     let
       inherit (specialArgs.inputs) nixpkgs;
-      inherit (nixpkgs.lib)
-        optional
-        evalModules
-        mkOption
-        types
-        ;
+      inherit (nixpkgs) lib;
+      inherit (nixpkgs.lib) mkOption types;
     in
-    evalModules (
-      args
-      // {
-        specialArgs = specialArgs // {
-          combinedManagerPath = ./.;
-        };
-        modules =
-          [
-            (
-              { config, ... }:
-              {
-                options = {
-                  # These inputs are used when generating the flake
-                  inputs = mkOption {
-                    type = inputType types;
-                    default = { };
-                    description = "Inputs";
-                  };
-
-                  osModules = mkOption {
-                    type = with types; listOf raw;
-                    default = [ ];
-                    description = "Top level NixOS modules.";
-                  };
-
-                  os = mkOption {
-                    type = types.submoduleWith {
-                      specialArgs.modulesPath = "${nixpkgs}/nixos/modules";
-                      modules = import "${nixpkgs}/nixos/modules/module-list.nix" ++ config.osModules;
-                    };
-                    default = { };
-                    visible = "shallow";
-                    description = "NixOS configuration.";
-                  };
-                };
-
-                config = {
-                  _module.args.osConfig = config.os;
-                  os.nixpkgs.system = system;
-                };
-              }
-            )
-          ]
-          ++ optional useHomeManager (
-            {
-              inputs,
-              options,
-              config,
-              osConfig,
-              ...
-            }:
+    lib.evalModules {
+      inherit prefix;
+      class = "combinedManager";
+      specialArgs = specialArgs // {
+        combinedManagerPath = ./.;
+        configs = { }; # TODO
+      };
+      modules =
+        [
+          (
+            { config, ... }:
             {
               options = {
-                hmUsername = mkOption {
-                  type = types.str;
-                  default = "user";
-                  description = "Username used for Home Manager.";
+                inputs = mkOption {
+                  type = getInputType types;
+                  default = { };
+                  description = "Inputs";
                 };
 
-                hmModules = mkOption {
+                osModules = mkOption {
+                  # A proper type
                   type = with types; listOf raw;
                   default = [ ];
-                  description = "Home Manager modules.";
+                  description = "NixOS modules.";
                 };
 
-                hm = mkOption {
-                  type = types.deferredModule;
+                os = mkOption {
+                  type = types.submoduleWith {
+                    specialArgs.modulesPath = "${nixpkgs}/nixos/modules";
+                    modules = import "${nixpkgs}/nixos/modules/module-list.nix" ++ osModules ++ config.osModules;
+                  };
                   default = { };
-                  description = "Home Manager configuration.";
+                  visible = "shallow";
+                  description = "NixOS configuration.";
                 };
               };
 
-              config = {
-                _module.args.hmConfig = osConfig.home-manager.users.${config.hmUsername};
-
-                osModules = [ inputs.home-manager.nixosModules.default ];
-
-                os.home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  extraSpecialArgs.inputs = inputs;
-                  sharedModules = config.hmModules;
-                  users.${config.hmUsername} = config.hm;
-                };
-              };
+              config._module.args.osConfig = config.os;
             }
           )
-          ++ modules;
-      }
-    );
+        ]
+        ++ lib.optional useHomeManager (
+          {
+            inputs,
+            config,
+            osConfig,
+            ...
+          }:
+          {
+            options = {
+              hmUsername = mkOption {
+                type = types.str;
+                default = "user";
+                description = "Username used for Home Manager.";
+              };
 
-  combinedManagerToNixosConfig = config: config // { config = config.config.os; };
+              hmModules = mkOption {
+                # A proper type
+                type = with types; listOf raw;
+                default = [ ];
+                description = "Home Manager modules.";
+              };
+
+              hm = mkOption {
+                # A proper type
+                type = types.deferredModule;
+                default = { };
+                description = "Home Manager configuration.";
+              };
+            };
+
+            config = {
+              _module.args.hmConfig = osConfig.home-manager.users.${config.hmUsername};
+
+              osModules = [ inputs.home-manager.nixosModules.default ];
+
+              os.home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs.inputs = inputs;
+                sharedModules = hmModules ++ config.hmModules;
+                users.${config.hmUsername} = config.hm;
+              };
+            };
+          }
+        )
+        ++ modules;
+    };
+
+  # TODO Correct options
+  combinedManagerToNixosConfig =
+    config:
+    config
+    // {
+      class = "nixos";
+      options = config.options.os;
+      config = config.config.os;
+    };
 in
 {
-  #inherit mkNixosSystem;
+  mkNixosSystem = args: combinedManagerToNixosConfig (evalModules args);
 
   mkFlake =
     {
@@ -144,52 +145,38 @@ in
 
       evalConfigInputs =
         config:
-        (evalModules (
-          config
-          // {
-            specialArgs.inputs.nixpkgs.lib = lib;
-            specialArgs.configs = { };
-          }
-        )).options.inputs.definitionsWithLocations;
+        (evalModules (config // { specialArgs.inputs.nixpkgs.lib = lib; }))
+        .options.inputs.definitionsWithLocations;
       inputsList = [
         ((builtins.unsafeGetAttrPos "initialInputs" args) // { value = initialInputs; })
       ] ++ lib.foldl (defs: config: defs ++ evalConfigInputs config) [ ] (lib.attrValues configurations);
-      inputs = (inputType lib.types).merge [ "inputs" ] inputsList;
+      inputs = (getInputType lib.types).merge [ "inputs" ] inputsList;
     in
     {
       inherit description inputs;
 
       outputs =
-        inputs:
+        args:
         let
-          explicitOutputs = outputs inputs;
+          explicitOutputs = outputs args;
 
-          allConfigurations = lib.mapAttrs (_: config: config.config) (
-            (lib.mapAttrs (
-              _: config:
-              builtins.trace config evalModules (
-                config
-                // {
-                  specialArgs = {
-                    inherit inputs;
-                    configs = allConfigurations;
-                  };
-                }
-              )
-              #mkCombinedManagerSystem {
-              #  configuration = config // {
-              #    specialArgs = (config.specialArgs or { }) // {
-              #      configs = allConfigurations;
-              #    };
-              #  };
-              #  inherit inputs;
-              #}
-            ) configurations)
-            // explicitOutputs.nixosConfigurations or { }
-          );
+          evalConfig =
+            config:
+            evalModules (
+              config
+              // {
+                specialArgs = {
+                  inputs = args;
+                  configs = allConfigs;
+                };
+              }
+            );
+          allConfigs =
+            (lib.mapAttrs (_: config: evalConfig config) configurations)
+            // explicitOutputs.nixosConfigurations or { };
         in
         {
-          nixosConfigurations = lib.mapAttrs (_: combinedManagerToNixosConfig) allConfigurations;
+          nixosConfigurations = lib.mapAttrs (_: combinedManagerToNixosConfig) allConfigs;
         }
         // explicitOutputs;
     };
