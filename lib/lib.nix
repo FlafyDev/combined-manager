f0b82efa80b5a919abd352a70cec9bb451d96508
Noah765 <noland62007@gmail.com>
2024-07-08 11:21:49 +0200

    Patching nixpkgs (unfinished)

--- lib/lib.nix
+++ lib/lib.nix
@@ -1,12 +1,22 @@
 lockFile:
 let
   inherit (builtins.fromJSON (builtins.readFile lockFile)) nodes;
-  nixpkgsLock = nodes.nixpkgs.locked;
-  flake = builtins.getFlake (
-    if (builtins.pathExists lockFile && nodes ? nixpkgs) then
-      "github:NixOS/nixpkgs/${nixpkgsLock.rev}"
-    else
-      "github:Nixos/nixpkgs/4284c2b73c8bce4b46a6adf23e16d9e2ec8da4bb"
-  );
+  useFixed = !builtins.pathExists lockFile || !nodes ? nixpkgs;
+  rev = if useFixed then "4284c2b73c8bce4b46a6adf23e16d9e2ec8da4bb" else nodes.nixpkgs.locked.rev;
+  sha256 = if useFixed then "" else nodes.nixpkgs.locked.narHash;
+
+  nixpkgsSrc = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
+
+  pkgs = import nixpkgsSrc { };
+
+  libDerivation = pkgs.stdenvNoCC.mkDerivation {
+    name = "patched-nixpkgs";
+    src = "${nixpkgsSrc}/lib";
+  };
+
+  output = import libDerivation;
 in
-flake.lib
+builtins.trace (builtins.attrNames output) output
