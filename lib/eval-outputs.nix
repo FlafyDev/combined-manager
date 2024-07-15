dd506eeeb0e8634a760ac47554170f2a71e8d78b
Noah765 <noland62007@gmail.com>
2024-07-15 09:44:59 +0200

    fix: inputOverrides

--- lib/eval-outputs.nix
+++ lib/eval-outputs.nix
@@ -8,73 +8,73 @@
   configurations,
   outputs ? (_: {}),
   ...
-}: rawInputs: let
-  inputs = (config.inputOverrides or (_: {})) inputs;
-in
-  with inputs.nixpkgs.lib; let
-    inherit (inputs.nixpkgs) lib;
-    modifiedLib = import ./modified-lib.nix lib;
-    evalModules = import ./eval-modules.nix;
+}: rawInputs:
+with rawInputs.nixpkgs.lib; let
+  inherit (rawInputs.nixpkgs) lib;
+  modifiedLib = import ./modified-lib.nix lib;
+  evalModules = import ./eval-modules.nix;
 
-    evalModule = configs: config: let
-      configModules = modifiedLib.collectModules null "" config.modules {
-        inherit lib inputs;
-        options = null;
-        config = null;
-      };
+  evalModule = configs: config: let
+    inputs = rawInputs // config.inputOverrides or (_: {}) rawInputs;
 
-      findImports = name: alias: x:
-        if x ? ${name} || x ? ${alias}
-        then x.${name} or [] ++ x.${alias} or []
-        else if x ? content
-        then findImports name alias x.content
-        else if x ? contents
-        then lib.foldl (imports: x: imports ++ findImports name alias x) [] x.contents
-        else [];
+    configModules = modifiedLib.collectModules null "" config.modules {
+      inherit lib inputs;
+      options = null;
+      config = null;
+    };
 
-      configOsModules = lib.foldl (defs: module: defs ++ findImports "osImports" "osModules" module.config) [] configModules;
-      configHmModules = lib.foldl (defs: module: defs ++ findImports "hmImports" "hmModules" module.config) [] configModules;
+    findImports = name: alias: x:
+      if x ? ${name} || x ? ${alias}
+      then x.${name} or [] ++ x.${alias} or []
+      else if x ? content
+      then findImports name alias x.content
+      else if x ? contents
+      then lib.foldl (imports: x: imports ++ findImports name alias x) [] x.contents
+      else [];
 
-      useHm = config.useHomeManager or useHomeManager;
-      module = evalModules {
-        system = config.system or defaultSystem;
-        prefix = config.prefix or [];
-        specialArgs = {inherit inputs useHm configs;} // globalSpecialArgs // config.specialArgs or {};
-        modules = globalModules ++ config.modules or [];
-        osModules = globalOsModules ++ config.osModules or [] ++ configOsModules ++ optional useHm inputs.home-manager.nixosModules.default;
-        hmModules = globalHmModules ++ config.hmModules or [] ++ configHmModules;
-      };
+    configOsModules = lib.foldl (defs: module: defs ++ findImports "osImports" "osModules" module.config) [] configModules;
+    configHmModules = lib.foldl (defs: module: defs ++ findImports "hmImports" "hmModules" module.config) [] configModules;
 
-      showWarnings = module:
-        foldl (
-          module: warning: builtins.trace "[1;31mwarning: ${warning}[0m" module
-        )
-        module
-        module.config.warnings;
+    useHm = config.useHomeManager or useHomeManager;
+    module = evalModules {
+      system = config.system or defaultSystem;
+      prefix = config.prefix or [];
+      specialArgs = {inherit inputs useHm configs;} // globalSpecialArgs // config.specialArgs or {};
+      modules = globalModules ++ config.modules or [];
+      osModules = globalOsModules ++ config.osModules or [] ++ configOsModules ++ optional useHm inputs.home-manager.nixosModules.default;
+      hmModules = globalHmModules ++ config.hmModules or [] ++ configHmModules;
+    };
 
-      showErrors = module: let
-        failedAssertions = lists.map (x: x.message) (filter (x: !x.assertion) module.config.assertions);
-      in
-        if failedAssertions == []
-        then module
-        else
-          throw ''
+    showWarnings = module:
+      foldl (
+        module: warning: builtins.trace "[1;31mwarning: ${warning}[0m" module
+      )
+      module
+      module.config.warnings;
 
-            Failed assertions:
-            ${concatStringsSep "\n" (map (x: "- ${x}") failedAssertions)}'';
+    showErrors = module: let
+      failedAssertions = lists.map (x: x.message) (filter (x: !x.assertion) module.config.assertions);
     in
-      showErrors (showWarnings module);
+      if failedAssertions == []
+      then module
+      else
+        throw ''
 
-    explicitOutputs = outputs inputs;
-    nixosConfigurations =
-      mapAttrs
-      (_: config:
-        config
-        // {
-          class = "nixos";
-          options = config.options.os;
-          config = config.config.os;
-        })
-      (let configs = mapAttrs (_: evalModule configs) configurations; in configs);
+          Failed assertions:
+          ${concatStringsSep "\n" (map (x: "- ${x}") failedAssertions)}'';
   in
-    explicitOutputs // {nixosConfigurations = nixosConfigurations // explicitOutputs.nixosConfigurations or {};}
+    showErrors (showWarnings module);
+
+  explicitOutputs = outputs inputs;
+  nixosConfigurations =
+    mapAttrs
+    (_: config:
+      config
+      // {
+        class = "nixos";
+        options = config.options.os;
+        config = config.config.os;
+      })
+    (let configs = mapAttrs (_: evalModule configs) configurations; in configs);
+in
+  explicitOutputs // {nixosConfigurations = nixosConfigurations // explicitOutputs.nixosConfigurations or {};}
